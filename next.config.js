cb3bedb67d756c560af7bcaffc24af60234ee560
Derek Lam <dereklam67@gmail.com>
2021-02-26 12:05:22 -0800

    test

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
+    exportPathMap: () => ({
+        '/': { page: '/' },
+      }),
     assetPrefix: isProd ? '/blog/' : ''
 }
