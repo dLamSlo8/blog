c8d2d69257274e0c61b7c4736256905b00369cf2
Derek Lam <dereklam67@gmail.com>
2021-02-26 12:04:07 -0800

    Updates

---
+++ _next/static/HKu8kbfA6n6fP0Udm7pPk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-71aff6ae5d41b72c3621.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-02a966d8f9b2aafcfe3b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
